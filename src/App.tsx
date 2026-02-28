de5ce0eb8bc3a9b5959e856c0565ac2450c4f183
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2026-02-28 07:51:11 +0530

    added loader to prevent white screen during routing based permissions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Loader from "./components/common/Loader";
+import { useAuth } from "./context/AuthProvider";
 import AppToastContainer from "./utils/AppToastContainer";
 
 function App({ children }: { children: React.ReactNode }) {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
       {children}
